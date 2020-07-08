7c87526c059e1de2b3dee8efcbff91c4da335fa3
abhishekbhatt209 <abhishekbhatt18@gnu.ac.in>
2020-07-08 15:19:05 +0530

    settings.py updated

--- awsproject/settings.py
+++ awsproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'awsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'awspro',
+        'USER': 'root',
+        'PASSWORD': 'abhishek6051#',
+        'HOST': 'database-1.cwh40dz66jzb.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
